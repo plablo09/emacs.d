b49df881a944b8765e6fb5a5543af53bdec02a88
plablo09 <pablo.lopez@centrogeo.edu.mx>
2016-04-26 07:53:51 -0500

    Configuración local (latex).

---
+++ lisp/init-local.el
@@ -0,0 +1,14 @@
+;;; package --- local settings
+;;; Commentary: Latex stuff
+
+;;; Code:
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+(provide 'init-local)
